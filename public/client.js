f97c3699c7a50ed3a3b1072b86ad0a4e276065b7
Glitch (cyber-text) <none>
2018-07-23 09:24:13 +0000

    Import of freeCodeCamp/boilerplate-socketio

--- public/client.js
+++ public/client.js
@@ -1,26 +1,15 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
-
-$(function() {
-  console.log('hello world :o')
+$( document ).ready(function() {
   
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams')
-    })
-  })
-
-  $('form').submit(function(event) {
-    event.preventDefault()
-    var dream = $('input').val()
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams')
-      $('input').val('')
-      $('input').focus()
-    })
-  })
-
-})
+  
+   
+  // Form submittion with new message in field with id 'm'
+  $('form').submit(function(){
+    var messageToSend = $('#m').val();
+    //send message to server here?
+    $('#m').val('');
+    return false; // prevent form submit from refreshing page
+  });
+  
+  
+  
+});
